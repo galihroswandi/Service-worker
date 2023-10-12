9b09c7d8946299d3ba77ca4395e99d1653b023d0
galihroswandi <galihroswandi25@gmail.com>
2023-10-12 14:29:10 +0700

    fix: service worker

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
  * Langkah pertama mendaftarkan service worker di navigator
  */
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/serviceWorker.js')
+    navigator.serviceWorker.register('../../serviceWorker.js')
     .then(registration => {
         console.log('SW registered', registration.scope);
     }).catch(err => {
