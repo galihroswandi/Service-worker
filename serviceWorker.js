03ef3d866c554167e1eabe05e4ece3b1c8c525c0
galihroswandi <galihroswandi25@gmail.com>
2023-10-12 14:32:10 +0700

    fix: update service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,16 @@
 /**
+ * Langkah pertama mendaftarkan service worker di navigator
+ */
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/serviceWorker.js')
+    .then(registration => {
+        console.log('SW registered', registration.scope);
+    }).catch(err => {
+        console.log('SW registration failed', err);
+    })
+}
+
+/**
  * Langkah kedua menyiapkan data yang akan di caching
  */
 const cache1 = "Learn-Cache0";
