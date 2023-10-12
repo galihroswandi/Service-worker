0061c77ac234d9d31d11ae31c8614f28a1cdcee4
galihroswandi <galihroswandi25@gmail.com>
2023-10-12 14:58:41 +0700

    apt: service worker

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Langkah pertama mendaftarkan service worker di navigator
+ */
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/serviceWorker.js")
+    .then((registration) => {
+      console.log("SW registered", registration.scope);
+    })
+    .catch((err) => {
+      console.log("SW registration failed", err);
+    });
+}
